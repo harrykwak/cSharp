f4479aafb5e8705b33ba9fcc5b6ee97ff4292aba
harry <harrykwak9595@gmail.com>
2021-05-06 10:59:43 +0900

    깃 단축키 테스트

--- forTest.cs
+++ forTest.cs
@@ -7,13 +7,13 @@ namespace cSharp
         public void Run()
         {
             int result = 0;
-            for (int i = 1; i < 6; i++)
+            for (int i = 1; i < 11; i++)
             {
-                for (int y = 1; y < 6; y++)
+                for (int y = 1; y < 11; y++)
                 {
                     Console.Write(y+result + "\t");
                 }
-                    result=result+5;
+                    result=result+10;
                     Console.WriteLine();
 
             }
